f1f2048bb8cd7bf2a7d1c97a3b9a53de2666e524
tpsyafiq <thoha.syafiq03@gmail.com>
2021-10-27 15:41:30 +0700

    pemrograman lanjut

---
+++ TugasLibraryUnitTest/TugasLibraryTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using TugasLibrary;
+using NUnit.Framework;
+
+namespace TugasLibraryUnitTest
+{
+    [TestFixture]
+    public class TugasLibraryTest
+    {
+        private User user;
+        private Day day;
+
+        [SetUp]
+        public void Init()
+        {
+            user = new User();
+            day = new Day();
+        }
+        [Test]
+        public void UserTest()
+        {
+            Assert.IsTrue(user.IsValidUser("admin", "admin"));
+        }
+        [Test]
+        public void DayTest()
+        {
+            Assert.AreEqual("Senin", day.NameOfDay(1));
+        }
+    }
+}
